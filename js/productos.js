f549f1a59320bc54a7ea275bd8e3e815ae5c6c92
Clementina Racciatti <clementinaracciatti@gmail.com>
2023-07-10 22:00:23 -0300

    Modificaciones semana 2 Julio

--- js/productos.js
+++ js/productos.js
@@ -5,13 +5,22 @@ btn1.onclick = mostrarlista1
 function mostrarlista1 () {
     document.getElementById('Datos1').innerHTML =
     `<div class="dProductos">
-    <img src="./img/Logo-BioFree.png">
-    <p class="textMainCentrado">BIOFREE es un exclusivo inoculante promotor de crecimiento compuesto por la combinación de Pseudomonas fluorescens y Azospirillum brasilense.</p>
+    <img src="./img/Logo-PowerTrop.png">
+    <p class="textMainCentrado">Bioestimulante y antiestresante 3% de ácido indol acético y aminoácidos. </p>
   </div>
   <div class="dProductos">
-    <img src="./img/Logo-Promotor.png">
-    <p class="textMainCentrado">Tratamiento biológico de semilla.De excelente acción: fungicida, PGPR y Nematicida. </p>
-  </div>`
+    <img src="./img/Logo-StimuTrop.png">
+    <p class="textMainCentrado">Fertilizante mineral mixto con características únicas de fisioactivación que combina la estimulación del desarrollo de las plantas.</p>
+  </div>
+  <div class="dProductos">
+  <img src="./img/Logo-BioCalda.png">
+  <p class="textMainCentrado">Fertilizante mineral foliar mixto, que contiene Nitrógeno y Boro. Una combinación equilibrada de aditivos, estabilizantes y aceites.</p>
+</div>
+<div class="dProductos">
+<img src="./img/Logo-Stamina.png">
+<p class="textMainCentrado">Bioestimulante y antiestresante. Por la formulación DOP, asegura una alta concentración de metabolitos ricos en aminoácidos, hormonas de crecimiento y nutrientes, provenientes de la
+fermentación de microorganismos.</p>
+</div>`
 }
 
 let btn2 = document.getElementById('btn2')
@@ -20,11 +29,19 @@ btn2.onclick = mostrarlista2
 function mostrarlista2 () {
     document.getElementById('Datos1').innerHTML =
     `<div class="dProductos">
-    <img src="./img/Logo-TotalNitro.png">
-    <p class="textMainCentrado"> Inoculante para soja con alta concentración de bacterias Brasyrhizobium Japonicum que aseguran la óptima fijación de Nitrógeno atmosférico por parte del cultivo.</p>
+    <img src="./img/Logo-BioFungi.png">
+    <p class="textMainCentrado"> Info</p>
   </div>
   <div class="dProductos">
-    <img src="./img/Logo-Protege.png">
+    <img src="./img/Logo-BioKato.png">
+    <p class="textMainCentrado">Bioinsecticida de excelencia para el control de chicharritas y chinches. Recomendado en el manejo integrado de plagas.</p>
+  </div>
+  <div class="dProductos">
+    <img src="./img/Logo-Bioexos.png">
+    <p class="textMainCentrado">info2</p>
+  </div>
+  <div class="dProductos">
+    <img src="./img/Logo-VCore.png">
     <p class="textMainCentrado">info2</p>
   </div>`
 }
@@ -35,21 +52,13 @@ btn3.onclick = mostrarlista3
 function mostrarlista3 () {
     document.getElementById('Datos1').innerHTML =
     `<div class="dProductos">
-    <img src="./img/Logo-BioFungi.png">
-    <p class="textMainCentrado">info</p>
+    <img src="./img/Logo-TotalNitro.png">
+    <p class="textMainCentrado">Inoculante para soja con alta concentración de bacterias Brasyrhizobium Japonicum que aseguran la óptima fijación de Nitrógeno atmosférico por parte del cultivo.</p>
   </div>
   <div class="dProductos">
-    <img src="./img/Logo-BioKato.png">
-    <p class="textMainCentrado">Bioinsecticida de excelencia para el control de chicharritas y chinches. Recomendado en manejo integrado de plagas.</p>
-  </div>
-  <div class="dProductos">
-  <img src="./img/Logo-Bioexos.png">
-  <p class="textMainCentrado">BIOEXOS es el bioinsecticida de excelencia en el manejo integrado de plagas. Compuesto por Azadiractina, su formulación permite la máxima eficacia.</p>
-</div>
-<div class="dProductos">
-<img src="./img/Logo-VCore.png">
-<p class="textMainCentrado">info4</p>
-</div>`
+    <img src="./img/Logo-Protege.png">
+    <p class="textMainCentrado">Protector de Brady. Factores NOD de reconocimiento bacteria-planta.  </p>
+  </div>`
 }
 
 let btn4 = document.getElementById('btn4')
@@ -58,15 +67,11 @@ btn4.onclick = mostrarlista4
 function mostrarlista4 () {
     document.getElementById('Datos1').innerHTML =
     `<div class="dProductos">
-    <img src="./img/Logo-Stimutrop.png">
-    <p class="textMainCentrado">STIMUTROP es un fertilizante mineral mixto con características únicas de fisioactivación.</p>
+    <img src="./img/Logo-Promotor.png">
+    <p class="textMainCentrado">Tratamiento biológico de semilla. De acción fungicida, PGPR y Nematicida.</p>
   </div>
   <div class="dProductos">
-    <img src="./img/Logo-BioCalda.png">
-    <p class="textMainCentrado">BIOCALDA es un fertilizante mineral foliar mixto, que contiene Nitrógeno y Boro.</p>
-  </div>
-  <div class="dProductos">
-  <img src="./img/Logo-Stamina.png">
-  <p class="textMainCentrado"> Antiestresante/recuperador de cultivos.Formulación exclusiva con aminoácidos de altísima calidad y nutrientes.</p>
-</div>`
+    <img src="./img/Logo-BioFree.png">
+    <p class="textMainCentrado">Exclusivo inoculante promotor del crecimiento compuesto por la combinación de Pseudomonas fluorescens y Azospirillum brasilense.</p>
+  </div>`
 }
